4278dbe8653f1e2b2dab1df2513cad25d671797a
habsho <zahedayham@gmail.com>
2020-02-11 12:44:23 +0100

    Updates

---
+++ precache-manifest.f1db59eb049fe93007f2179b207d9cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7422679ef44d48dce9631e35223b999e",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "150d0ce19a2dea84b789",
+    "url": "/recipes/static/css/main.42fb0880.chunk.css"
+  },
+  {
+    "revision": "0adfc21ec4792a42dd28",
+    "url": "/recipes/static/js/2.ebc11154.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipes/static/js/2.ebc11154.chunk.js.LICENSE"
+  },
+  {
+    "revision": "150d0ce19a2dea84b789",
+    "url": "/recipes/static/js/main.4ba13f22.chunk.js"
+  },
+  {
+    "revision": "e79679a2e36d7733aae2",
+    "url": "/recipes/static/js/runtime-main.ac0e6f80.js"
+  }
+]);
